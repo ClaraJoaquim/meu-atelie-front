6646b67226be64b6bc474f5714fe2e76de422236
ClaraJoaquim <clarajpmarques@gmail.com>
2026-02-22 17:36:23 -0300

    FEAT: Adicionar footer, header e modal

---
+++ src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+
+declare var bootstrap: any;
+
+@Component({
+  selector: 'app-modal',
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css'
+})
+export class ModalComponent {
+@ViewChild('meuModal') modalRef!: ElementRef;
+  @Output() acaoConfirmada = new EventEmitter<void>(); // Emite evento quando clica no botão principal
+
+  titulo: string = '';
+  mensagem: string = '';
+  tipo: 'sucesso' | 'erro' = 'sucesso'; // Controla a cor
+  textoBotao: string = 'OK';
+  
+  private modalBootstrap: any;
+
+  // Método que o Pai vai chamar para abrir o modal
+  abrirModal(titulo: string, mensagem: string, tipo: 'sucesso' | 'erro', textoBotao: string = 'OK') {
+    this.titulo = titulo;
+    this.mensagem = mensagem;
+    this.tipo = tipo;
+    this.textoBotao = textoBotao;
+
+    // Inicializa e mostra o modal
+    this.modalBootstrap = new bootstrap.Modal(this.modalRef.nativeElement);
+    this.modalBootstrap.show();
+  }
+
+  fecharModal() {
+    this.modalBootstrap?.hide();
+  }
+
+  confirmar() {
+    this.acaoConfirmada.emit(); // Avisa o pai que o botão foi clicado
+    this.fecharModal();
+  }
+}
