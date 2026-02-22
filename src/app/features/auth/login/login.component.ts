d1f75c8b42c03b8fb3ca3ba1a61492150dbad56b
ClaraJoaquim <clarajpmarques@gmail.com>
2026-02-22 17:31:13 -0300

    feat: adicionar tela de login

---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
