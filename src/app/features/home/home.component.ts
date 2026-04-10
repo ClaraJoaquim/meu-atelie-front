d0d5d5d6d647a65e3c19a1aff59a70c180dc0cb2
ClaraJoaquim <clarajpmarques@gmail.com>
2026-04-10 11:40:24 -0300

    feat: adicionar tela de home

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [FooterComponent, HeaderComponent, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
