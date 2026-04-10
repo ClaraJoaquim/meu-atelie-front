82cb163745afc286782776b27f29b05903b1e133
ClaraJoaquim <clarajpmarques@gmail.com>
2026-04-10 11:45:06 -0300

    feat: adicionar token e autenticação de login

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { FormsModule } from '@angular/forms';
+import { ModalComponent } from '../../../shared/components/modal/modal.component';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, ModalComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -13,6 +14,7 @@ export class LoginComponent {
 
   email: string = '';
   senha: string = '';
+  @ViewChild(ModalComponent) modal!: ModalComponent;
 
   constructor(
     private authService: AuthService,
@@ -23,12 +25,28 @@ export class LoginComponent {
     this.authService.login(this.email, this.senha)
       .subscribe({
         next: () => {
-          console.log('Login ok');
+          this.modal.abrirModal(
+          'Sucesso!',
+          'Login efetuado com sucesso!',
+          'sucesso',
+          'Continuar'
+        );
+
+        this.modal.acaoConfirmada.subscribe(() => {
           this.router.navigate(['/home']);
+        });
         },
         error: (err) => {
-          console.error(err);
-          alert('Email ou senha inválidos');
+          if (this.modal) {
+          this.modal.abrirModal(
+            'Erro',
+            'E-mail ou senha incorreto',
+            'erro',
+            'Fechar'
+          );
+        } else {
+          console.error('Modal não inicializado', err);
+        }
         }
       });
   }
---
+++ src/app/features/auth/services/token.service.ts
@@ -0,0 +1,141 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  
+  public jwtPayload: any = '';
+  private servidor: string;
+
+  constructor(private http: HttpClient,
+    private JwtHelper: JwtHelperService,
+    private servidorService: ServidorService,
+    private router: Router) {
+    this.servidor = this.servidorService.getServidor() + '/oauth/token';
+    this.carregarToken();
+  }
+
+  /**
+   * 
+   * @param token 
+   * 
+   * @description
+   * Armazena o token retornado do backend na memória do browser do usuário.
+   * @returns void;
+   */
+  public armazenarToken(token: string): void {
+    this.jwtPayload = this.JwtHelper.decodeToken(token);
+    localStorage.removeItem('token');
+    localStorage.setItem('token', token);
+  }
+
+  /**
+   * @description
+   * Carrega o token na memoria do browser do usuario.
+   * 
+   * @see armazenarToken
+   */
+  public carregarToken(): void {
+    const token = localStorage.getItem('token');
+
+    if (token != null) {
+      if (token.length > 1) {
+        this.armazenarToken(token);
+      }
+    }
+
+  }
+
+  /**
+   * @description Verifica se token é válido ou não
+   */
+  public isAccessTokenInvalido(): boolean {
+    const token = localStorage.getItem('token');
+
+    return !token || this.JwtHelper.isTokenExpired(token);
+  }
+
+  /**
+   * @description Verifica se token expirou
+   */
+  public verificarTokenExpirado() {
+    const token = localStorage.getItem('token');
+    return this.JwtHelper.isTokenExpired(token);
+  }
+
+  /**
+   * @description 
+   * Verifica o access token está definido ou não.
+   */
+  public verificarTokenDefinido() {
+    const token = localStorage.getItem('token');
+    if (token != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * 
+   * @param permissao 
+   * 
+   * @description
+   * Verifica se o token do usuário tem a permissão passada por parametro
+   * 
+   * @returns token && boolean
+   */
+  public temPermissao(permissao: string): any {
+    return this.jwtPayload && this.jwtPayload.authorities.includes(permissao);
+  }
+
+  /**
+   * @description
+   * Requisita um novo access token para o backend
+   * 
+   * @returns void;
+   */
+  public requisitarNovoAccessToken(): Promise<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic YWRtaW46QWRNaU4='
+    });
+    const body = 'grant_type=refresh_token';
+
+    return this.http.post<any>(this.servidor, body, { headers, withCredentials: true })
+      .toPromise()
+      .then(response => {
+        this.armazenarToken(response.access_token);
+        return response;
+      })
+      .catch(response => {
+        return response;
+      });
+  }
+
+  public limparAccessToken() {
+    localStorage.removeItem('token');
+    this.jwtPayload = null;
+  }
+
+  public logout() {
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    const token = localStorage.getItem('token');
+    if (token) {
+      headers = headers.set('Authorization', 'Bearer ' + token);
+    }
+
+    this.http.delete(this.servidorService.getServidor() + '/tokens/revoke', { headers }).toPromise()
+      .finally(() => {
+        this.limparAccessToken();
+        localStorage.clear();
+
+        this.router.navigate(['/login']);
+      });
+  }
+}
