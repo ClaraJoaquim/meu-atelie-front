6fee43bfc5283659b3a45601d2cda4aa62dbb163
ClaraJoaquim <clarajpmarques@gmail.com>
2026-08-16 18:06:43 -0300

    refactor: atualizar rotas de clientes, produtos e pedidos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,13 @@ import { MateriaisComponent } from './features/materiais/materiais.component';
 import { ClientesComponent } from './features/clientes/clientes.component';
 import { PedidosComponent } from './features/pedidos/pedidos.component';
 import { RelatoriosComponent } from './features/relatorios/relatorios.component';
-import { EncomendasComponent } from './features/encomendas/encomendas.component';
+
 import { CadastroClienteComponent } from './features/cadastro-cliente/cadastro-cliente.component';
 import { CadastroMateriaisComponent } from './features/cadastro-materiais/cadastro-materiais.component';
 import { ProdutosComponent } from './features/produtos/produtos.component';
 import { CadastroProdutosComponent } from './features/cadastro-produtos/cadastro-produtos.component';
 import { PerfilComponent } from './features/perfil/perfil.component';
+import { CadastroPedidosComponent } from './features/cadastro-pedidos/cadastro-pedidos.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
@@ -22,10 +23,13 @@ export const routes: Routes = [
     { path: 'pedidos', component: PedidosComponent},
     { path: 'produtos', component: ProdutosComponent},
     { path: 'relatorios', component: RelatoriosComponent},
-    { path: 'encomenda', component: EncomendasComponent},
+    { path: 'pedidos/cadastro', component: CadastroPedidosComponent},
+    { path: 'pedidos/editar/:id', component: CadastroPedidosComponent},
     { path: 'clientes/cadastro', component: CadastroClienteComponent},
+    { path: 'clientes/editar/:id', component: CadastroClienteComponent },
     { path: 'materiais/cadastro', component: CadastroMateriaisComponent},
     { path: 'produtos/cadastro', component: CadastroProdutosComponent},
+    { path: 'produtos/editar/:id', component: CadastroProdutosComponent },
     { path: 'meu-perfil', component: PerfilComponent},
     { path: '', redirectTo: 'login', pathMatch: 'full'},
 ];
