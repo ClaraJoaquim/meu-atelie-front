2bcbca20edec4d7a6f511419f6bfa9315d96abfa
ClaraJoaquim <clarajpmarques@gmail.com>
2026-08-16 18:06:38 -0300

    feat: exibir dashboard de pedidos na home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,14 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { PedidoService } from '../../core/services/pedido.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent, HeaderComponent, RouterModule],
+  imports: [FooterComponent, HeaderComponent, RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  dashboard: any = null;
+  saudacao: string = '';
 
+  constructor(private pedidoService: PedidoService) {}
+
+  ngOnInit(): void {
+    this.definirSaudacao();
+    this.pedidoService.obterDashboard().subscribe({
+      next: (dados) => this.dashboard = dados,
+      error: (erro) => console.error('Erro ao carregar dashboard', erro)
+    });
+
+    this.pedidoService.obterDashboard().subscribe({
+      next: (dados) => {
+        const ordenados = dados.pedidosRecentes.sort((a: any, b: any) => (a.id ?? 0) - (b.id ?? 0));
+        
+        const comIdVisual = ordenados.map((pedido: any, index: number) => ({
+          ...pedido,
+          idVisual: index + 1
+        }));
+        
+        dados.pedidosRecentes = comIdVisual.slice(-5);
+        
+        this.dashboard = dados;
+      },
+      error: (erro) => console.error('Erro ao carregar dashboard', erro)
+    });
+  }
+
+  obterStatusAjustado(statusObj: any): string {
+    if (typeof statusObj === 'string') return statusObj.toUpperCase();
+    if (!statusObj || !statusObj.descricao) return '';
+    return statusObj.descricao
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .toUpperCase()
+      .replace(' ', '_');
+  }
+
+getClasseStatus(statusObj: any): string {
+    const statusFormatado = this.obterStatusAjustado(statusObj);
+    switch (statusFormatado) {
+      case 'EM_ANDAMENTO': return 'status--progress';
+      case 'FINALIZADO': return 'status--done';
+      case 'ORCAMENTO': return 'status--orcamento';
+      case 'CANCELADO': 
+      case 'ATRASADO': return 'status--late';
+      default: return 'status--progress';
+    }
+  }
+
+  getLabelStatus(statusObj: any): string {
+    if (statusObj && statusObj.descricao) {
+      return statusObj.descricao;
+    }
+    
+    const statusFormatado = this.obterStatusAjustado(statusObj);
+    const mapa: any = {
+      'EM_ANDAMENTO': 'Em andamento',
+      'FINALIZADO': 'Finalizado',
+      'ATRASADO': 'Atrasado',
+      'CANCELADO': 'Cancelado',
+      'ORCAMENTO': 'Orçamento'
+    };
+    return mapa[statusFormatado] || statusFormatado;
+  }
+
+  definirSaudacao() {
+  const hora = new Date().getHours();
+  if (hora >= 6 && hora < 12) {
+    this.saudacao = 'Bom dia';
+  } else if (hora >= 12 && hora < 19) {
+    this.saudacao = 'Boa tarde';
+  } else {
+    this.saudacao = 'Boa noite';
+  }
+}
 }
