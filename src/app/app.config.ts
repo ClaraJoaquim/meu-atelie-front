c11193771de82aff1af7ed449e274ab82a12f5dd
ClaraJoaquim <clarajpmarques@gmail.com>
2026-08-16 18:06:17 -0300

    feat: configurar locale pt-BR na aplicacao

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './features/auth/auth-interceptor';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID } from '@angular/core';
+
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
 };
